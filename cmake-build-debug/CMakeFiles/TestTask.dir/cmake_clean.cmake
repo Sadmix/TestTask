47bc10dc181895ff1e68d7d62f39044848700e3a
Dmitry Safronov <dmitsfrnv@gmai.com>
2019-08-02 19:12:14 +0300

    Created an empty project

---
+++ cmake-build-debug/CMakeFiles/TestTask.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestTask.dir/main.cpp.o"
+  "TestTask.pdb"
+  "TestTask"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestTask.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
