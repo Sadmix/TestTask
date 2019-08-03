8202bce9b01684c7673ca585359eda3e03464707
Sadmix <dmitsfrnv@gmail.com>
2019-08-03 23:22:07 +0300

    Added a not naive solution

--- main.cpp
+++ main.cpp
@@ -47,7 +47,22 @@ uint32_t ssum(uint32_t left, uint32_t right){
 
 };
 
+uint32_t ssum2(uint32_t left, uint32_t right){
+
+    uint32_t signmask = 0x80808080;
+    uint32_t t0 = (right ^ left) & signmask; // не общие старшие разряды
+    uint32_t t1 = (right & left) & signmask; // общие старшие разряды
+    left &= ~signmask; // убираем старшие разряды,
+    right &= ~signmask; // чтобы не было переполнения при сложении
+    left += right; // складываем
+    t1 |= t0 & left; // добавление старших разрядов появившихся при сложении
+    t1 = (t1 << 1) - (t1 >> 7); // ?
+    return (left ^ t0) | t1; // зачем ксорить ?
+
+};
+
 int main() {
+    std::cout << ssum(0x290198, 0x982901) << std::endl;
     std::cout << ssum(0x290198, 0x982901) << std::endl;
     return 0;
 }
